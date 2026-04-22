8130d2f3cea719831d106fb4ac5738ca6e8bd858
FelishaDwina <felishadwina2005@mail.ugm.ac.id>
2026-04-22 23:20:33 +0700

    API GeoJSON

--- app/Models/PolygonsModel.php
+++ app/Models/PolygonsModel.php
@@ -3,10 +3,39 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class PolygonsModel extends Model
 {
     protected $table = 'polygons';
     protected $guarded = ['id'];
+
+    public function geojson_polygons()
+    {
+        $polygons = $this-> select(DB::raw('id, ST_AsGeoJSON(geom) as geojson, name,
+        description, created_at, updated_at'))->get();
+        $geojson = [
+        'type' => 'FeatureCollection',
+        'features' => []
+    ];
+
+    //perulangan setiap polygon dan buat fitur GeoJSON
+    foreach ($polygons as $p) {
+        $feature = [
+        'type' => 'Feature',
+        'geometry' => json_decode($p->geojson),
+        'properties' => [
+            'id' => $p->id,
+            'name' => $p->name,
+            'description' => $p->description,
+            'created_at' => $p->created_at,
+            'updated_at' => $p->updated_at,
+            'image' => $p->image,
+        ]
+    ];
+    array_push($geojson['features'], $feature);
+    }
+    return $geojson;
+    }
 }
 
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -185,6 +185,92 @@ map.on('draw:created', function(e) {
 	drawnItems.addLayer(layer);
 
 });
+
+//point layer
+var points = L.geoJSON(null, {
+	// Style
+
+	// onEachFeature
+    onEachFeature: function (feature, layer) {
+	// variable popup content
+	var popup_content = "Nama: " + feature.properties.name + "<br>" +
+		"Deskripsi: " + feature.properties.description + "<br>" +
+		"Dibuat: " + feature.properties.created_at;
+
+	layer.on({
+		click: function (e) {
+			points.bindPopup(popup_content);
+		},
+	});
+},
+});
+
+$.getJSON("{{route('geojson.points')}}", function(data) {
+	points.addData(data);
+	map.addLayer(points);
+});
+
+//polyline layer
+var polylines = L.geoJSON(null, {
+	// Style
+
+	// onEachFeature
+    onEachFeature: function (feature, layer) {
+	// variable popup content
+	var popup_content = "Nama: " + feature.properties.name + "<br>" +
+		"Deskripsi: " + feature.properties.description + "<br>" +
+		"Dibuat: " + feature.properties.created_at;
+
+	layer.on({
+		click: function (e) {
+			polylines.bindPopup(popup_content);
+		},
+	});
+},
+});
+
+$.getJSON("{{route('geojson.polylines')}}", function(data) {
+	polylines.addData(data);
+	map.addLayer(polylines);
+});
+
+//polygon layer
+var polygons = L.geoJSON(null, {
+	// Style
+
+	// onEachFeature
+    onEachFeature: function (feature, layer) {
+	// variable popup content
+	var popup_content = "Nama: " + feature.properties.name + "<br>" +
+		"Deskripsi: " + feature.properties.description + "<br>" +
+		"Dibuat: " + feature.properties.created_at;
+
+	layer.on({
+		click: function (e) {
+			polygons.bindPopup(popup_content);
+		},
+	});
+},
+});
+
+$.getJSON("{{route('geojson.polygons')}}", function(data) {
+	polygons.addData(data);
+	map.addLayer(polygons);
+});
+
+// Control Layer
+var baseMaps = {
+
+};
+
+var overlayMaps = {
+	"Points": points,
+	"Polylines": polylines,
+	"Polygons": polygons,
+};
+
+var controllayer = L.control.layers(baseMaps, overlayMaps);
+controllayer.addTo(map);
 </script>
 
 @endsection
